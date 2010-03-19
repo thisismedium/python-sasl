4f72d2449f92c533a4a05bf8f596971d7757b0b9
Ben Weaver <weaver@coptix.com>
2010-03-19 17:46:37 -0400

    Bump version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'python-sasl',
-    version = '0.1',
+    version = '0.1.1',
     description = 'A SASL implementation.',
     author = 'Medium',
     author_email = 'labs@thisismedium.com',
